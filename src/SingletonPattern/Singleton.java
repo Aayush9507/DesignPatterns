1d7f0c18c9cb5e20d6ecd9574f35685c27827ac6
aayush goyal <aayushgoyal9507@gmail.com>
2019-06-07 17:38:03 -0600

    Singleton pattern

---
+++ src/SingletonPattern/Singleton.java
@@ -0,0 +1,18 @@
+package SingletonPattern;
+
+public class Singleton {
+
+    private static Singleton uniqueInstance;
+
+    private Singleton(){};
+
+    public static synchronized Singleton getInstance(){
+        if (uniqueInstance==null){
+            uniqueInstance = new Singleton();
+        }
+
+            return uniqueInstance;
+    }
+
+}
+
